9a327a2c6562d7be9c85157f4119b2f63f74b613
kushal <collabskus@gmail.com>
2025-12-05 15:18:44 -0500

    add initial repository

---
+++ source/HelloDotnetTen/HelloDotnetTen.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+var env = builder.Environment;
+
+Console.WriteLine(env.EnvironmentName);
+
+if (env.IsDevelopment())
+{
+    Console.WriteLine("Development environment");
+}
+else
+{
+    Console.WriteLine("Production environment");
+}
