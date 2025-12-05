6081fe4b39da575a7d17a41dd5a847f9ca79678c
kushal <collabskus@gmail.com>
2025-12-05 15:22:25 -0500

    add class library

---
+++ source/HelloDotnetTen/HelloDotnetTen.ClassLibrary1/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloDotnetTen.ClassLibrary1
+{
+    public class Class1
+    {
+        private readonly string injectedProperty1;
+        public Class1(string injectedProperty1)
+        {
+            this.injectedProperty1 = injectedProperty1 ?? throw new ArgumentNullException(nameof(injectedProperty1));
+        }
+        public int GetLengthOfInjectedProperty1()
+        {
+            return injectedProperty1.Length;
+        }
+    }
+}
---
+++ source/HelloDotnetTen/HelloDotnetTen.ClassLibrary1/Class2.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloDotnetTen.ClassLibrary1
+{
+    public class Class2
+    {
+        private readonly string injectedProperty1;
+        public Class2(string injectedProperty1)
+        {
+            this.injectedProperty1 = injectedProperty1 ?? throw new ArgumentNullException(nameof(injectedProperty1));
+        }
+        public int GetLengthOfInjectedProperty1()
+        {
+            return injectedProperty1.Length;
+        }
+    }
+}
